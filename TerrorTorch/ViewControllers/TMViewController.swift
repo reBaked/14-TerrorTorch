14a48c7509bd266770030adfb4b00f1b512dbedd
mitrenegade <bobbyren@gmail.com>
2014-07-06 14:22:27 -0400

    added countdown to terror mode

--- TerrorTorch/ViewControllers/TMViewController.swift
+++ TerrorTorch/ViewControllers/TMViewController.swift
@@ -8,12 +8,51 @@
 
 import UIKit
 
-class TMViewController: UIViewController {
+
+protocol Ticker {
+    func Tick(timeLeft:Double)
+    func Timeout()
+}
+
+class CountdownTimerModel :NSObject {
+    var timeLeft:Double
+    var timer:NSTimer?
+    var delegate:AnyObject
+
+    init(initialTime:Double, delegate:AnyObject) {
+        timeLeft = initialTime
+        self.delegate = delegate
+    }
+
+    func startCountdown() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: "tick", userInfo: nil, repeats: true)
+    }
+
+    func tick() {
+        timeLeft -= 1
+        self.delegate.Tick(timeLeft)
+        if (timeLeft == 0) {
+            self.timer?.invalidate()
+            self.delegate.Timeout()
+        }
+    }
+}
+
+class TMViewController: UIViewController, Ticker {
+
+    @IBOutlet var labelCountdown:UILabel
+    var countdown: CountdownTimerModel?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+
+        // start countdown
+        let totalTime:Double = 15
+        self.labelCountdown.text = String(totalTime)
+        self.countdown = CountdownTimerModel(initialTime:totalTime, delegate:self)
+        self.countdown?.startCountdown()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -24,7 +63,6 @@ class TMViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
     /*
     // #pragma mark - Navigation
@@ -36,4 +74,14 @@ class TMViewController: UIViewController {
     }
     */
 
+    // Ticker delegate
+    func Tick(timeLeft:Double) {
+        NSLog("Tick: \(timeLeft)")
+        self.labelCountdown.text = String(timeLeft)
+    }
+
+    func Timeout() {
+        NSLog("Done")
+        self.labelCountdown.text = "Starting camera"
+    }
 }
