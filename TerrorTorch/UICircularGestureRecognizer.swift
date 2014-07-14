35bfc80aa760236d9cd3e8be15a3130b5e58fb37
Michael Honaker <mike@beachcitiessoft.com>
2014-07-14 16:24:40 -0600

    - Added logic to MainViewController to limit "knob" control rotation between -90 and 90 - Calculate and set torch intensity/level based on "knob" control value - Added doc blocks

--- TerrorTorch/UICircularGestureRecognizer.swift
+++ TerrorTorch/UICircularGestureRecognizer.swift
@@ -5,37 +5,28 @@
 //  Created by Michael Honaker on 7/10/14.
 //  Copyright (c) 2014 reBaked. All rights reserved.
 //
-//
-// Usage From a view controller:
-// ====================================================
-//    override func viewDidLoad() {
-//        ...
-//        let circularRecognizer:UICircularGestureRecognizer = UICircularGestureRecognizer(target: self, action: "rotated:");
-//        self.view.addGestureRecognizer(circularRecognizer);
-//    }
-//    func rotated(recognizer: UICircularGestureRecognizer) {
-//        UICircularGestureRecognizer.rotateView(recognizer);
-//    }
 
 import Foundation
 import UIKit
 
-
 class UICircularGestureRecognizer: UIGestureRecognizer {
     
-    var rotation:CGFloat = 0.0;
+    /**
+    *   Tracks current rotation value
+    */
+    var rotation:Float = 0.0;
     
     override func touchesBegan(touches: NSSet!, withEvent event: UIEvent!) {
         if event.touchesForGestureRecognizer(self).count > 1 {
-            self.state = UIGestureRecognizerState.Failed;
+            self.state = .Failed;
         }
     }
     
     override func touchesEnded(touches: NSSet!, withEvent event: UIEvent!) {
         if UIGestureRecognizerState.Changed == self.state {
-            self.state = UIGestureRecognizerState.Ended;
+            self.state = .Ended;
         } else {  
-            self.state = UIGestureRecognizerState.Failed;
+            self.state = .Failed;
         }
     }
     
@@ -44,10 +35,10 @@ class UICircularGestureRecognizer: UIGestureRecognizer {
     }
     
     override func touchesMoved(touches: NSSet!, withEvent event: UIEvent!)  {
-        if UIGestureRecognizerState.Possible == self.state {
-            self.state = UIGestureRecognizerState.Began;
+        if .Possible == self.state {
+            self.state = .Began;
         } else {
-            self.state = UIGestureRecognizerState.Changed;
+            self.state = .Changed;
         }
         
         let touch = touches.anyObject() as UITouch;
@@ -55,27 +46,47 @@ class UICircularGestureRecognizer: UIGestureRecognizer {
         let center = CGPointMake(CGRectGetMidX(view.bounds), CGRectGetMidY(view.bounds)); // rotation around center axis
         let current = touch.locationInView(view);
         let previous = touch.previousLocationInView(view);
-        self.rotation = CGFloat(atan2f(Float(current.y - center.y), Float(current.x - center.x)) - atan2f(Float(previous.y - center.y), Float(previous.x - center.x)));
+        let radians:Float = atan2f(Float(current.y - center.y), Float(current.x - center.x)) - atan2f(Float(previous.y - center.y), Float(previous.x - center.x));
+        self.rotation = radians;
     }
     
     //==========================================================================
     // CLASS METHODS
     //==========================================================================
     
+    /**
+    *   Rotates the view attached to the gesture recognizer based on the recognizers current rotation
+    *   @param circularRecognizer:UICircularGestureRecognizer
+    */
     class func rotateView(circularRecognizer: UICircularGestureRecognizer!) {
         let view:UIView = circularRecognizer.view;
         view.transform = CGAffineTransformRotate(view.transform, circularRecognizer.rotation);
     }
     
-    class func rotateView(view: UIView!, degrees: CGFloat) {
+    /**
+    *   Rotates the passed view to the specified degree (degree is relative to view's current rotation)
+    *   @param view:UIView
+    *   @param degrees:Float A degree value between -180.0 and 180.0
+    */
+    class func rotateView(view: UIView!, degrees: Float) {
         view.transform = CGAffineTransformRotate(view.transform, UICircularGestureRecognizer.degreesToRadians(degrees));
     }
     
-    class func degreesToRadians(degrees: CGFloat) -> CGFloat {
-        return (degrees * CGFloat(M_PI) / 180);
+    /**
+    *   Converts between degrees and radians
+    *   @param degrees:Float
+    *   @return Float
+    */
+    class func degreesToRadians(degrees: Float) -> Float {
+        return (Float(M_PI) * degrees / 180.0);
     }
     
-    class func radiansToDegrees(radians: CGFloat) -> CGFloat {
-        return ((radians * 180) / CGFloat(M_PI));
+    /**
+    *   Converts between radians and degrees
+    *   @param radians:Float
+    *   @return Float
+    */
+    class func radiansToDegrees(radians: Float) -> Float {
+        return (radians * 180.0 / Float(M_PI));
     }
 }
