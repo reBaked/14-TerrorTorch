8a4bb5b7d428e0d76b5614a577c6f8e343ba256d
Alfred Jeremy Cepeda <AlfredJCepeda@gmail.com>
2014-09-01 21:24:33 -0700

    Added createVideo and a way to authenticate user through curl

--- cloudcode/cloud/main.js
+++ cloudcode/cloud/main.js
@@ -13,43 +13,88 @@ function setOptionals(object, params, fields){
 }
 
 function hasRequired(params, fields){
-	var error = "Missing"
+	var error = { 
+		code:107,
+		message:'Missing field(s):'
+	}
 	var failed = false;
 	for(var i = 0; i < fields.length; i++){
 		if(!isDefined(params[fields[i]])){
-			error = error + " " + fields[i];
+			error.message = error.message + ' ' + fields[i];
 			failed = true;
 		}	
 	}
 
 	if(failed){
-		var result = false;
-		result.error = error;
-		return result;
+		return {hasRequired:false, error:error};
 	} else {
-		return true;
+		return {hasRequired:true};
 	}
 }
 
-Parse.Cloud.define("hello", function(request, response){
+function attemptLogin(params, callback){
+	var result = new Parse.Promise();
+
+	if(isDefined(params['sessiontoken'])){
+		result = Parse.User.become(params.sessiontoken);
+	} 
+
+	else if(!isDefined(params['username']) && !isDefined(params['email'])){
+		result = Parse.Promise.error({
+			code:204,
+			message:'Must provide a username or email'
+		});
+	} 
+
+	else if(!isDefined(params['password'])){
+		result = Parse.Promise.error({
+			code:200,
+			message:'Must provide a password or valid sessiontoken'
+		});
+	} 
+
+	else {
+		//Username or email must have been provided
+		var loginname = (isDefined(params['username'])) ? params.username : params.email;
+		result = Parse.User.logIn(params.username, params.password);
+	}
+
+	if(isDefined(callback)){
+		if(isDefined(callback.success)){
+			result.done(function(user){
+				callback.success(user);
+			});
+		}
+
+		if(isDefined(callback.error)){
+			result.fail(function(error){
+				callback.error(null, error);
+			});
+		}
+	}
+
+	return result;
+} 
+
+Parse.Cloud.define('hello', function(request, response){
 	var firstname = request.params['firstname'];
 	var lastname = request.params['lastname'];
 	if(typeof firstname !== 'undefined' && typeof lastname !== 'undefined'){
-		response.success("Hello " + firstname + " " + lastname + ", congratulations on sending a request to our TerrorTorch backend." );
+		response.success('Hello ' + firstname + ' ' + lastname + ', congratulations on sending a request to our TerrorTorch backend.' );
 	} else {
 		response.error("I'm sorry I don't talk to strangers");
 	}
 });
 
-Parse.Cloud.define("createUser", function(request, response){
-	var meetsRequired = hasRequired(request.params, ['username', 'email', 'password', 'vendorID']);
+Parse.Cloud.define('createUser', function(request, response){
+	var params = hasRequired(request.params, ['username', 'email', 'password', 'vendorid']);
 
-	if(meetsRequired){
+	if(params.hasRequired == true){
 		var user = new Parse.User();
-		user.setUsername(request.params['username']);
-		user.setPassword(request.params['password']);
-		user.setEmail(request.params['email']);
-		user.set("vendorID",request.params['vendorID']);
+		user.setUsername(request.params.username);
+		user.setPassword(request.params.password);
+		user.setEmail(request.params.email);
+		user.set('vendorid',request.params.vendorid);
 
 		setOptionals(user, request.params, ['firstname', 'lastname']);
 
@@ -62,37 +107,89 @@ Parse.Cloud.define("createUser", function(request, response){
  			}
 		});
 	} else {
-		response.error(meetsRequired.error);
+		response.error(params.error.code + ' ' + params.error.message);
 	}
 });
 
-Parse.Cloud.define("loginUser", function(request, response){
-	var username = request.params['username'];
-	var password = request.params['password'];
-
-	var meetsRequired = hasRequired(request.params, ['username', 'password']);
-	if(meetsRequired){
-		Parse.User.logIn(request.params['username'], request.params['password'], {
+Parse.Cloud.define('loginUser', function(request, response){
+	var params = hasRequired(request.params, ['username', 'password']);
+	
+	if(params.hasRequired){
+		attemptLogin(request.params, {
 			success: function(user){
 				response.success({
 					firstname:user.get('firstname'),
-					lastname:user.get('lastname')
+					lastname:user.get('lastname'),
+					sessionToken:user.getSessionToken()
 				});
-			},
+			}, 
 			error: function(user, error){
-				response.error(error.code + " " + error.message);
+				response.error(error.code + ' ' + error.message);
 			}
-		})
+		});
 	} else {
-		response.error(meetsRequired.error);
+		response.error(params.error.code + ' ' + params.error.message);
 	}
 });
 
+Parse.Cloud.define('createVideo', function(request, response){
+	var params = hasRequired(request.params, ['title', 'length']);
+	var promise = Parse.Promise.as();
 
+	if(!params.hasRequired){
+		response.error(params.error.code + ' ' + params.error.message);
+		return;
+	}
+
+	if(request.user == null){
+		promise = attemptLogin(request.params);
+	}
+
+	promise.done(function(user){
+		var VideoClass = Parse.Object.extend('Video');
+		var video = new VideoClass();
+		video.set('title', request.params.title);
+		video.set('length', request.params.length);
+
+		setOptionals(video, request.params, ['description', 'videoData']);
+
+		return video.save({
+			success: function(video){
+				response.success('Video was successfully saved');
+			},
+			error: function(video, error){
+				response.error(error.code + ' ' + error.message);
+			}
+		});
+	});
+
+	promise.fail(function(error){
+		response.error(error.code + ' ' + error.message);
+	});	
+});
+
+Parse.Cloud.beforeSave('Video', function(request, response){
+	if(request.master){
+		response.success();
+		return;
+	}
+
+	if(request.user == null){
+		response.error('You must be authenticated before saving a video');
+		return;
+	}
+
+	if(!request.object.get("title")){
+		response.error("Video must have a title");
+	} else {
+		request.object.set("createdBy", request.user);
+		response.success();
+	}
+})
 
 Parse.Cloud.beforeSave(Parse.User, function(request, response) {
-	if(!request.object.get("vendorID")){
-		response.error("User's vendorId is required for signup");
+	if(!request.object.get('vendorid')){
+		response.error("User's vendorID is required for signup");
 	} else {
 		response.success();
 	}
