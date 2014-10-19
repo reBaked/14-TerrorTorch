b295179b67f0eeeae07095914b3ed9f80812464d
Alfred Jeremy Cepeda <AlfredJCepeda@gmail.com>
2014-10-19 02:23:43 -0700

    Improved settings page

--- TerrorTorch/Managers/Global Context.swift
+++ TerrorTorch/Managers/Global Context.swift
@@ -12,10 +12,10 @@
 let SOUNDFORMAT:String = "wav";
 
 // Color Constants in HEX
-let COLOR_RED:UIColor = UIColor(hexColor:0xC11D25);
-let COLOR_BLACK:UIColor = UIColor(hexColor:0x040404);
-let COLOR_WHITE:UIColor = UIColor(hexColor:0xFFFAFA);
-let COLOR_GREY:UIColor = UIColor(hexColor:0x2C2E2D);
+let COLOR_RED:UIColor = UIColor(hexColor:0xC11D25);  //R:193 G:29 B:37
+let COLOR_BLACK:UIColor = UIColor(hexColor:0x040404);//R:04 G:04 B:04
+let COLOR_WHITE:UIColor = UIColor(hexColor:0xFFFAFA);//R:255 G:250 B:250
+let COLOR_GREY:UIColor = UIColor(hexColor:0x2C2E2D); //R:44 G:46 B:45
 
 // Logo Constants
 let FONT_TITLE:UIFont = UIFont(name: "HelveticaNeue", size: 20.0);
